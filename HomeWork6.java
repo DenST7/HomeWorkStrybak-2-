badb31d0c142991397936f08f3059ff121231716
DenST7 <mister.denst@yandex.ru>
2016-09-29 13:55:20 +0300

    Update HomeWork6.java

--- HomeWork6.java
+++ HomeWork6.java
@@ -13,12 +13,12 @@ public class HomeWork6 {
             }
             System.out.println(" ");
         }
-        int sum = Math.abs(a[0][0] * a[0][1] * a[0][2] * a[0][3]);
+        int sum = a[0][0] * a[0][1] * a[0][2] * a[0][3];
         int max = 0;
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 4; j++) {
-                if (sum < Math.abs(a[i][0] * a[i][1] * a[i][2] * a[i][3]))
-                    sum = Math.abs(a[i][0] * a[i][1] * a[i][2] * a[i][3]);
+                if (sum < a[i][0] * a[i][1] * a[i][2] * a[i][3])
+                    sum = a[i][0] * a[i][1] * a[i][2] * a[i][3];
                 max = i;
             }
         }
